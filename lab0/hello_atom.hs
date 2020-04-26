aca142965652771eecb997ee56b7cbe4dedc32c0
Irina Lee <irinalee@uchicago.edu>
2020-04-26 17:28:21 -0400

    Print out my info

--- lab0/hello_atom.hs
+++ lab0/hello_atom.hs
@@ -20,3 +20,14 @@ why = "Because I love to solve problems"
 
 distance :: Int -> Int -> Int
 distance rate time = rate * time
+
+main :: IO ()
+main = do
+    putStrLn "Hello, world!"
+    putStrLn ""
+    putStrLn ("My name is " ++ name ++ ".")
+    putStrLn ("I am a " ++ show level ++ " student.")
+    putStrLn ("I'm going into " ++ major ++ ".")
+    putStrLn ("I'm in this class because " ++ why)
+    putStrLn ""
+    putStrLn ("If you travel for 15mph for 30 hours you will go " ++ show (distance 15 30) ++ " miles.")
